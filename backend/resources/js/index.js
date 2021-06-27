bcf28471331c129cbbaa41650b20c791f54b74c3
arakakitatsuki <20yuteo@gmail.com>
2021-06-27 23:53:55 +0900

    fix index js for using react redux

--- backend/resources/js/index.js
+++ backend/resources/js/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import createStore from './reducks/store/store';
+import {ConnectedRouter} from "connected-react-router";
+import * as History from 'history';
 import App from './views/App';
 
+const history = History.createBrowserHistory();
+
+export const store = createStore(history);
+
 if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
+    ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('app'));
 }
