66de8481afe9209be39ee571d7c1c59f9bb15338
arakakitatsuki <20yuteo@gmail.com>
2021-06-27 23:54:31 +0900

    fix login jsx for login proccess

--- backend/resources/js/views/pages/Login.jsx
+++ backend/resources/js/views/pages/Login.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useDispatch} from 'react-redux';
+import { signIn } from '../../reducks/users/operations';
 
 const Login = () => {
+
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <div>
-            Login
+            <h2>Login</h2>
+            <form>
+            <label>email</label>
+            <input
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+            />
+            <label>password</label>
+            <input
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
+            <button type="button" onClick={() => dispatch(signIn(email, password))}>
+                ログインする
+            </button>
+            </form>
         </div>
     );
 }
