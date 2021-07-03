d3e6ea461ceecb5f435af755b81d7d358f94bfe8
arakakitatsuki <20yuteo@gmail.com>
2021-07-03 21:46:19 +0900

    fix size js to add font size

--- backend/resources/js/views/styles/size.js
+++ backend/resources/js/views/styles/size.js
@@ -1,10 +1,14 @@
 const Size = {
+    FontSize: {
+        Small: 0.8,
+        Medium: 1.6,
+        Large: 2.4
+    },
     Button = {
         BorderRadius: 3,
         PaddingTop: 1.2 ,
         PaddingLeft: 2,
         Opacity: 1,
-        FontSize: 1.1,
         FontWeight: 900,
     }
 }
