b95f71a1fdb43efc139ccf457d0da0306f92d487
arakakitatsuki <20yuteo@gmail.com>
2021-07-03 15:55:58 +0900

    add size js for styles

---
+++ backend/resources/js/views/styles/size.js
@@ -0,0 +1,11 @@
+const Size = {
+    Button = {
+        BorderRadius: 3,
+        PaddingTop: 1.2 ,
+        PaddingLeft: 2,
+        Opacity: 1,
+        FontSize: 1.1,
+        FontWeight: 900,
+    }
+}
+export default Size
