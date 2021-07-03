b9675460c0aafeef818e9828541722ff6be734fa
arakakitatsuki <20yuteo@gmail.com>
2021-07-03 23:57:46 +0900

    add password form jsx

---
+++ backend/resources/js/views/molecules/PasswordForm.jsx
@@ -0,0 +1,18 @@
+import BaseFormInput from '../atoms/Forms/BaseFormInput';
+import BaseFormLabel from '../atoms/Forms/BaseFormLabel';
+
+const PasswordForm = ({password, setPassword}) => {
+    return (
+        <>
+            <BaseFormLabel>
+                Password
+            </BaseFormLabel>
+            <BaseFormInput
+                type="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+            />
+        </>
+    )
+}
+export default PasswordForm;
