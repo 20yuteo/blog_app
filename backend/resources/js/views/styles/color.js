c83f36774c5bd220b909ae7373829f6dc2a68fad
arakakitatsuki <20yuteo@gmail.com>
2021-07-03 15:55:45 +0900

    add color js for styles

---
+++ backend/resources/js/views/styles/color.js
@@ -0,0 +1,30 @@
+const Color = {
+    Background: '#fef6e4',
+    Headline: '#001858',
+    Paragraph: '#172c66',
+    Button: '#f582ae',
+    ButtonText: '#001858',
+    Stroke: '#001858',
+    Main: '#f3d2c1',
+    Highlight: '#fef6e4',
+    Secondary: '#8bd3dd',
+    Tertiary: '#f582ae',
+    Link: '#f582ae',
+    Label: '#001858',
+    PlaceHolder: '#001858',
+    Card: {
+        Background: '#001858',
+        Headline: '#fffffe',
+        Paragraph: '#fef6e4',
+        TagBackground: '#fef6e4',
+        TagText: '#001858',
+        Highlight: '#f582ae',
+    },
+    Form: {
+        Input: '#f3d2c1',
+        Button: '#f582ae',
+        ButtonText: '#001858',
+    }
+}
+export default Color
+// https://www.happyhues.co/palettes/17
