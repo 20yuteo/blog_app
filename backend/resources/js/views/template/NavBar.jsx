b8b24bd4193ceec6ccadea285cb1d376bbda282d
arakakitatsuki <20yuteo@gmail.com>
2021-07-04 13:06:14 +0900

    fix nav bar jsx for responsive

--- backend/resources/js/views/template/NavBar.jsx
+++ backend/resources/js/views/template/NavBar.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Size from '../styles/size';
 
 const NavBar = () => {
+
+    const Links = styled.div`
+        display: flex;
+        width: 8rem;
+        @media screen and (min-width:${Size.MediaScreen.Small}px) {
+            flex-flow: column;
+        }
+    `;
+
     return (
-        <div>
+        <Links>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/login">Login</Link>
-        </div>
+        </Links>
     );
 }
 
