2d650a31daa9f1cb7a0852c0b0cd90c87457dbf2
arakakitatsuki <20yuteo@gmail.com>
2021-07-04 13:06:49 +0900

    fix app jsx for responsive

--- backend/resources/js/views/App.jsx
+++ backend/resources/js/views/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import NavBar from './template/NavBar';
 import Router from './Router';
+import Size from './styles/size';
 
 const App = () => {
+    const Main = styled.div`
+        display: flex;
+        @media screen and (max-width:${Size.MediaScreen.Small}px) {
+            flex-direction: column-reverse;
+        }
+    `;
+
     return(
-        <>
+        <Main>
+            <NavBar />
             <Router />
-        </>
+        </Main>
     )
 }
 export default App;
