026ecb7a7573b7bd0755ccc9b840989c79bafe8b
arakakitatsuki <20yuteo@gmail.com>
2021-07-04 13:06:35 +0900

    fix router jsx for responsive

--- backend/resources/js/views/Router.jsx
+++ backend/resources/js/views/Router.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
 import { Route, Switch } from 'react-router';
-import NavBar from './template/NavBar';
 import {Login, About, Home} from './pages';
 
 const Router = () => {
+    const Main = styled.div`
+        flex: 1;
+    `;
+
     return (
-        <>
-            <NavBar />
+        <Main>
             <Switch>
                 <Route exact path={"/login"} component={Login} />
                 <Route exact path={"/about"} component={About} />
                 <Route exact path={"(/)?"} component={Home} />
             </Switch>
-        </>
+        </Main>
     )
 }
 export default Router
