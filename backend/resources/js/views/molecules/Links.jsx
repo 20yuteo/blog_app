8b725cb1613cf4942f86f54fb216b9553700ca07
arakakitatsuki <20yuteo@gmail.com>
2021-07-10 22:20:54 +0900

    add links.jsx

---
+++ backend/resources/js/views/molecules/Links.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import Color from "../styles/color";
+import Size from "../styles/size";
+
+const Links = styled.div`
+    display: flex;
+    width: 100%;
+    border-top: 8px solid ${Color.Stroke};
+    overflow:scroll;
+    background: ${ Color.SideBar.Background };
+    @media screen and (min-width:${Size.MediaScreen.Small}px) {
+        flex-flow: column;
+        border-right: 8px solid ${Color.Stroke};
+        width: 16rem;
+        height: 100%;
+    }
+`;
+
+export default Links;
