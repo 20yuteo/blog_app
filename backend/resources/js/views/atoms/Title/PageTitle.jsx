b2c900ba0029ce373d5041ddd7c51ef0ab6cd3cf
arakakitatsuki <20yuteo@gmail.com>
2021-07-11 15:34:31 +0900

    add page title jsx

---
+++ backend/resources/js/views/atoms/Title/PageTitle.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Size from "../../styles/size";
+import Color from "../../styles/color";
+
+const PageTitle = styled.div`
+    color: ${ Color.Headline };
+    font-size: ${ Size.FontSize.Large }rem;
+    font-weight: ${ Size.FontWeight };
+    padding: 3rem;
+`;
+
+export default PageTitle;
