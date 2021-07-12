9ae89b08949feacad500ece266ee4ddcd4bf414f
arakakitatsuki <20yuteo@gmail.com>
2021-07-12 13:06:45 +0900

    fix api routing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,8 +15,10 @@ use App\Http\Controllers\LoginController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::get('user', function(Request $request){
+        return $request->user();
+    });
 });
 
 Route::post('/login', [LoginController::class, 'login']);
