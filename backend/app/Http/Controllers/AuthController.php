11ad2c130d5f24ea3935e3358c8775fdd0d052fd
arakakitatsuki <20yuteo@gmail.com>
2021-07-14 01:12:56 +0900

    fix name of login controller and add get login user action

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -8,13 +8,19 @@ use App\Repositories\User\UserRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\LoginUserRequest;
 
-class LoginController extends Controller
+class AuthController extends Controller
 {
     use AuthenticatesUsers;
 
     public function __construct(UserRepositoryInterface $user)
     {
         $this->user = $user;
+    }
+
+    public function getLoginUser()
+    {
+        $auth_response = $this->user->getAuthUser();
+        return response()->json($auth_response);
     }
 
     public function login(LoginUserRequest $request)
