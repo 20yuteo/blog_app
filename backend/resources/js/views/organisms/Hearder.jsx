7b4d1698c31fb64099c03cade34b696b53ee8a4f
arakakitatsuki <20yuteo@gmail.com>
2021-07-13 19:25:06 +0900

    fix to use logo png at header component

--- backend/resources/js/views/organisms/Hearder.jsx
+++ backend/resources/js/views/organisms/Hearder.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Logo from "../../../../public/image/logo.png"
+import Logo from "../../../../public/images/logo.png"
 import { BaseLogoImage } from "../atoms/Image/BaseLogoImage";
 import { Link } from "react-router-dom";
 import Color from "../styles/color";
