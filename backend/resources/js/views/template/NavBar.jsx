3d63f4e43574bd8b1a96c793ca9143d29255ee51
arakakitatsuki <20yuteo@gmail.com>
2021-07-13 19:21:20 +0900

    fix navbar component to use link wrapper js

--- backend/resources/js/views/template/NavBar.jsx
+++ backend/resources/js/views/template/NavBar.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Links from '../molecules/Links';
 import BaseLink from '../atoms/Links/BaseLink';
+import { LinkWrapper } from '../molecules/LinkWrapper';
+import BaseButton from '../atoms/buttons/BaseButton';
 import Color from '../styles/color';
 
 const NavBar = () => {
 
     return (
         <Links>
-            <BaseLink to={ '/' } backgroundColor={ Color.Background } borderColor={ Color.Stroke } Name={'Home'}/>
-            <BaseLink to={ '/about' } backgroundColor={ Color.Secondary } borderColor={ Color.Stroke }  Name={'About'} />
-            <BaseLink to={ '/login' } backgroundColor={ Color.Tertiary } borderColor={ Color.Stroke } Name={'Login'} />
+            <LinkWrapper>
+                <BaseLink to={ '/' } backgroundColor={ Color.Background } borderColor={ Color.Stroke } Name={'Home'}/>
+            </LinkWrapper>
+            <LinkWrapper>
+                <BaseLink to={ '/about' } backgroundColor={ Color.Secondary } borderColor={ Color.Stroke }  Name={'About'} />
+            </LinkWrapper>
+            <LinkWrapper>
+                <BaseLink to={ '/login' } backgroundColor={ Color.Tertiary } borderColor={ Color.Stroke } Name={'Login'} />
+            </LinkWrapper>
+            <LinkWrapper>
+                <BaseButton type="button" minWidth={8} minHeight={2} paddingTop={1.2} paddingLeft={3}>Logout</BaseButton>
+            </LinkWrapper>
         </Links>
     );
 }
