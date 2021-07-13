45488e3a08aee48115dc6290dbe977b0725c3fea
arakakitatsuki <20yuteo@gmail.com>
2021-07-14 00:29:46 +0900

    add logout routing for users

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,3 +22,4 @@ Route::group(['middleware' => ['auth:sanctum']], function(){
 });
 
 Route::post('/login', [LoginController::class, 'login']);
+Route::get('/logout', [LoginController::class, 'logout']);
