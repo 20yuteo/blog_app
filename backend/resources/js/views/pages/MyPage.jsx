53acc2297ca8937b949fab95bdec025b923a5c4a
arakakitatsuki <20yuteo@gmail.com>
2021-07-15 15:20:59 +0900

    add my page component

---
+++ backend/resources/js/views/pages/MyPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageTitle from '../atoms/Title/PageTitle';
+
+const MyPage = () => {
+    return (
+        <PageTitle>
+            MyPage
+        </PageTitle>
+    );
+}
+
+export default MyPage
