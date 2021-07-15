eec8aeb55eddd8382d6b44f600dfeaec43a9ef8c
arakakitatsuki <20yuteo@gmail.com>
2021-07-15 15:21:33 +0900

    add routing of my page to router jsx

--- backend/resources/js/views/Router.jsx
+++ backend/resources/js/views/Router.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import { Route, Switch } from 'react-router';
-import {Login, About, Home} from './pages';
+import {Login, About, Home, MyPage} from './pages';
 
 const Router = () => {
     const Nav = styled.nav`
@@ -14,6 +14,7 @@ const Router = () => {
                 <Route exact path={"/login"} component={Login} />
                 <Route exact path={"/about"} component={About} />
                 <Route exact path={"(/)?"} component={Home} />
+                <Route exact path={"/my_page"} component={MyPage} />
             </Switch>
         </Nav>
     )
