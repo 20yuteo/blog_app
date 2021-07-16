fe96e38c69e0a0cbeb94ceb18152ee6afc1dc5a7
arakakitatsuki <20yuteo@gmail.com>
2021-07-17 00:35:04 +0900

    add element js

---
+++ backend/resources/js/views/styles/element.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import Size from "./size";
+
+export const Main = styled.main`
+    display: flex;
+    height: 100%;
+    @media screen and (max-width:${Size.MediaScreen.Small}px) {
+        flex-direction: column-reverse;
+    }
+`;
+
+export const Section = styled.section`
+    display: flex;
+    flex-flow: column;
+    flex-grow: 1;
+`;
