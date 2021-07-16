6acf69c6fd79739ff732635945e8ce69a8e5c062
arakakitatsuki <20yuteo@gmail.com>
2021-07-17 00:32:13 +0900

    add styled loader js

---
+++ backend/resources/js/views/atoms/Loader/StyledLoader.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import Loader from "react-loader-spinner";
+
+const StyledLoader = styled(Loader)`
+    margin: 8rem auto;
+`;
+
+export default StyledLoader;
