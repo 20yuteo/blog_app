355ed206c7c645978c6d59033001f31508796c82
arakakitatsuki <20yuteo@gmail.com>
2021-07-19 20:08:28 +0900

    add error boundary component to app jsx

--- backend/resources/js/views/App.jsx
+++ backend/resources/js/views/App.jsx
@@ -5,6 +5,7 @@ import Router from './Router';
 import Header from './organisms/Hearder';
 import { Section } from './styles/element';
 import { Main } from './styles/element';
+import { ErrorBoundary } from '../ErrorBoundary';
 import { getLoginUser } from '../reducks/users/operations';
 import StyledLoader from './atoms/Loader/StyledLoader';
 import Color from './styles/color';
@@ -23,7 +24,9 @@ const App = () => {
         <Main>
             <NavBar />
             <Section>
-                { selector.loading.isLoading ? <StyledLoader type="Puff" color={ Color.Stroke } height={80} width={80} /> : <><Header/><Router users={selector.users} /></> }
+                <ErrorBoundary>
+                    { selector.loading.isLoading ? <StyledLoader type="Puff" color={ Color.Stroke } height={80} width={80} /> : <><Header/><Router users={selector.users} /></> }
+                </ErrorBoundary>
             </Section>
         </Main>
     )
