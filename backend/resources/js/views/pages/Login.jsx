f7db4ed7ec78903ccc66515fc396d9d07357fbbc
arakakitatsuki <20yuteo@gmail.com>
2021-07-19 20:10:05 +0900

    delete use state at login jsx

--- backend/resources/js/views/pages/Login.jsx
+++ backend/resources/js/views/pages/Login.jsx
@@ -1,24 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch } from 'react-redux';
 import PageTitle from '../atoms/Title/PageTitle';
 import LoginForm from '../organisms/LoginForm';
 
 const Login = () => {
-
     const dispatch = useDispatch();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
     return (
         <div>
             <PageTitle>
                 Login
             </PageTitle>
             <LoginForm
-                email={email}
-                password={password}
-                setEmail={setEmail}
-                setPassword={setPassword}
                 dispatch={dispatch}
             />
         </div>
