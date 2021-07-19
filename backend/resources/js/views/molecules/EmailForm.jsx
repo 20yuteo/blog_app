2492655eb48f948df252b2aa589cbb05da24f66f
arakakitatsuki <20yuteo@gmail.com>
2021-07-19 22:39:00 +0900

    fix to add error component to email form

--- backend/resources/js/views/molecules/EmailForm.jsx
+++ backend/resources/js/views/molecules/EmailForm.jsx
@@ -13,6 +13,7 @@ const EmailForm = ({register}) => {
                 marginBottom={8}
                 {...register("email", {
                     required: true,
+                    pattern: /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}.[A-Za-z0-9]{1,}$/,
                 })}
             />
         </>
