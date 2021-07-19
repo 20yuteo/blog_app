710225b638d9583f961c14a21c76ef2284ad3e63
arakakitatsuki <20yuteo@gmail.com>
2021-07-19 20:09:23 +0900

    fix base link to give props

--- backend/resources/js/views/atoms/Links/BaseLink.js
+++ backend/resources/js/views/atoms/Links/BaseLink.js
@@ -1,11 +1,14 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 import Color from '../../styles/color';
 import Size from '../../styles/size';
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(({ backgroundColor, Name, ...props }) => <Link {...props} />)`
+    ${(p = props) =>
+        css`
+        background-color: ${p.backgroundColor ? p.backgroundColor : ''};
+        `}
     display: inline-block;
-    background-color: ${props => props.backgroundColor};
     color: ${Color.ButtonText};
     font-size: ${Size.FontSize.Medium}rem;
     font-weight: ${Size.Form.FontWeight};
@@ -18,7 +21,7 @@ const StyledLink = styled(Link)`
 
 const BaseLink = (props) => {
     return (
-        <StyledLink to={props.to} backgroundColor={props.backgroundColor} borderColor={props.borderColor}>{props.Name}</StyledLink>
+        <StyledLink to={props.to} backgroundColor={props.backgroundColor} >{props.Name}</StyledLink>
     )
 }
 export default BaseLink;
