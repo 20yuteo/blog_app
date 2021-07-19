711ccca2f76dfdc2ae320576fc0186163b95966d
arakakitatsuki <20yuteo@gmail.com>
2021-07-19 20:11:39 +0900

    fix login form component to add use form

--- backend/resources/js/views/organisms/LoginForm.jsx
+++ backend/resources/js/views/organisms/LoginForm.jsx
@@ -2,14 +2,24 @@ import EmailForm from '../molecules/EmailForm';
 import PasswordForm from '../molecules/PasswordForm';
 import BaseButton from '../atoms/buttons/BaseButton';
 import BaseForm from './BaseForm';
+import { useForm } from "react-hook-form";
 import { signIn } from '../../reducks/users/operations';
 
-const LoginForm = ({email, password, setEmail, setPassword, dispatch}) => {
+const LoginForm = ({dispatch}) => {
+
+    const { register, watch, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = () => {
+        dispatch(signIn(watch('email'), watch('password')))
+    };
+
     return (
-        <BaseForm PaddingTop={16}>
-            <EmailForm email={email} setEmail={setEmail} />
-            <PasswordForm password={password} setPassword={setPassword} />
-            <BaseButton type="button" paddingTop={1.2} paddingLeft={2} onClick={() => dispatch(signIn(email, password))}>
+        <BaseForm PaddingTop={16} onSubmit={handleSubmit(onSubmit)}>
+            <EmailForm register={register} />
+            {errors.email && errors.email.type === "required" && <span>This is required</span>}
+            <PasswordForm register={register} />
+            {errors.password && errors.password.type === "required" && <span>This is required</span>}
+            <BaseButton type="submit" paddingTop={1.2} paddingLeft={2}>
                 ログイン
             </BaseButton>
         </BaseForm>
