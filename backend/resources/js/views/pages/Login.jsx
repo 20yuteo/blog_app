6f31129aed50a34697960ae5ea931acb233acfa6
arakakitatsuki <20yuteo@gmail.com>
2021-07-20 10:01:14 +0900

    fix login component to delete useDispatch

--- backend/resources/js/views/pages/Login.jsx
+++ backend/resources/js/views/pages/Login.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
 import PageTitle from '../atoms/Title/PageTitle';
 import LoginForm from '../organisms/LoginForm';
 
 const Login = () => {
-    const dispatch = useDispatch();
     return (
         <div>
             <PageTitle>
                 Login
             </PageTitle>
-            <LoginForm
-                dispatch={dispatch}
-            />
+            <LoginForm />
         </div>
     );
 }
