6c48d28911dc9b5c12dd8cedde78fe87079bd43c
arakakitatsuki <20yuteo@gmail.com>
2021-07-26 00:31:31 +0900

    add post component

---
+++ backend/resources/js/views/pages/Post.jsx
@@ -0,0 +1,12 @@
+import PageTitle from "../atoms/Title/PageTitle"
+
+const Post = () => {
+    return (
+        <>
+            <PageTitle>
+                Post
+            </PageTitle>
+        </>
+    )
+}
+export default Post;
