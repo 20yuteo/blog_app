117b9c3303ce2f97aee6e4b85277fe72da88a3a9
arakakitatsuki <20yuteo@gmail.com>
2021-07-25 23:30:38 +0900

    add modal profile image js

---
+++ backend/resources/js/views/atoms/Image/ModalProfileImage.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { BaseProfileImage } from "./BaseProfileImage";
+
+const ModalProfileImage = styled(BaseProfileImage)`
+    max-width: 16rem;
+    max-height: 16rem;
+`;
+export default ModalProfileImage;
