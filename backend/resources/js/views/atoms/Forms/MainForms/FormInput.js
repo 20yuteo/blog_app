06cdb34543934fe035d8899b749b9dca4ba21a19
arakakitatsuki <20yuteo@gmail.com>
2021-07-25 23:30:12 +0900

    add form input js

---
+++ backend/resources/js/views/atoms/Forms/MainForms/FormInput.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import Color from "../../../styles/color";
+import BaseFormInput from "../BaseFormInput";
+
+const FormInput = styled(BaseFormInput)`
+    color: ${ Color.Form.Color };
+`;
+export default FormInput;
