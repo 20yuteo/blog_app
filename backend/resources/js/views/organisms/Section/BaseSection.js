176612ccfcde041dc41698939f2f48af0a513e5d
arakakitatsuki <20yuteo@gmail.com>
2021-07-26 00:31:51 +0900

    add base section js

---
+++ backend/resources/js/views/organisms/Section/BaseSection.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import Size from "../../styles/size";
+
+const BaseSection = styled.section`
+    justify-content: center;
+    margin: 0 auto;
+    @media screen and (max-width:${Size.MediaScreen.Small}px) {
+        max-width: 32rem;
+    }
+    @media screen and (min-width:${Size.MediaScreen.Small}px) and ( max-width:${Size.MediaScreen.Medium}px) {
+        max-width: 32rem;
+    }
+    @media screen and (min-width:${Size.MediaScreen.Large}px) {
+        max-width: 64rem;
+    }
+`;
+export default BaseSection;
