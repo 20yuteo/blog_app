d3f6c79b73bce6e42830e6d5cd9ccc478c5f55cf
arakakitatsuki <20yuteo@gmail.com>
2021-07-25 23:32:18 +0900

    add section js

---
+++ backend/resources/js/views/organisms/Profile/Section.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Color from "../../styles/color";
+import Size from "../../styles/size";
+
+const Section = styled.section`
+    z-index:2;
+    width: 80%;
+    height: 80%;
+    padding: 1em;
+    background:${ Color.Card.Background };
+    color: ${ Color.Card.Headline };
+    font-size: ${ Size.FontSize.Medium }rem;
+`;
+export default Section;
