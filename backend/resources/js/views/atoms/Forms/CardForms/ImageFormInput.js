1cef9ae6891e4fad57fa70e27cde51e16081ddaf
arakakitatsuki <20yuteo@gmail.com>
2021-07-25 23:29:31 +0900

    add image form input js

---
+++ backend/resources/js/views/atoms/Forms/CardForms/ImageFormInput.js
@@ -0,0 +1,7 @@
+import BaseFormInput from "../BaseFormInput";
+import styled from "styled-components";
+
+const ImageFormInput = styled(BaseFormInput)`
+    display: none;
+`;
+export default ImageFormInput;
