f402eb08ea7bc3d57461083f9b85428b5b14dcc5
arakakitatsuki <20yuteo@gmail.com>
2021-07-30 19:30:34 +0900

    fix operations of posts js

--- backend/resources/js/reducks/posts/operations.js
+++ backend/resources/js/reducks/posts/operations.js
@@ -2,12 +2,11 @@ import { addPostAction } from "./actions";
 import { push } from "connected-react-router";
 import axios from "axios";
 
-export const addPost = (tag_name) => {
+export const addPost = (tag_name, tagsArray, mainContent) => {
     return async(dispatch, getState) => {
         if (tag_name !== undefined){
-            axios.post('api/post', { tag_name })
+            axios.post('api/post', { tag_name, tagsArray, mainContent })
                 .then(res => {
-                    console.log(res);
                     dispatch(addPostAction({
                         tags_name: res.posts.tags_name,
                     }))
