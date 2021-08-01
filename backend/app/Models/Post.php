d528b4cd3870fee572af622820da5ead4ea8b18e
arakakitatsuki <20yuteo@gmail.com>
2021-08-01 21:38:27 +0900

    add content column to fillable

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -9,6 +9,10 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'content'
+    ];
+
     public function users()
     {
         return $this->belongsTo('App\Models\User');
