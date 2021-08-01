3fc3160dbec327be28e5144abe021c2f64a0115d
arakakitatsuki <20yuteo@gmail.com>
2021-08-01 21:52:07 +0900

    fix style of base form section js

--- backend/resources/js/views/molecules/Card/BaseFormSection.js
+++ backend/resources/js/views/molecules/Card/BaseFormSection.js
@@ -5,12 +5,18 @@ const BaseFormSection = styled.section`
     display: flex;
     flex-flow: column;
     align-items: center;
-    margin: 8px;
+    margin: auto;
+    text-align: center;
+    @media screen and (max-width:${Size.MediaScreen.Small}px) {
+            width: 270px;
+        }
     @media screen and (min-width:${Size.MediaScreen.Small}px) and ( max-width:${Size.MediaScreen.Medium}px) {
         min-height: 260px;
+        width: 500px;
     }
     @media screen and (min-width:${Size.MediaScreen.Large}px) {
         min-height: 260px;
+        width: 750px;
     }
 `;
 export default BaseFormSection;
