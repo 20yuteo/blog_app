1e27622ceb7bf9b5d083a0d0389421a6259160be
arakakitatsuki <20yuteo@gmail.com>
2021-08-02 18:31:52 +0900

    delete console.log at tags wrapper component

--- backend/resources/js/views/molecules/TagsWrapper.jsx
+++ backend/resources/js/views/molecules/TagsWrapper.jsx
@@ -12,7 +12,6 @@ export const TagsWrapper = ({tag_array, register, watch}) => {
 
     if (tag_array !== undefined && tag_array.length > 0){
         for (let i = 0; i < tag_array.length; i ++){
-            console.log(tag_array[i]);
             tagSectionArray.push(<TagsCheckBox name={tag_array[i]['name']} register={register} watch={watch} key={i}　/>);
         }
     }
