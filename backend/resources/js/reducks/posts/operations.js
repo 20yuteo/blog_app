2ec0ae9d18f550f44aa59eb3141b72f43f5e43bc
arakakitatsuki <20yuteo@gmail.com>
2021-08-10 18:17:46 +0900

    fix posts operations js to fetch post title

--- backend/resources/js/reducks/posts/operations.js
+++ backend/resources/js/reducks/posts/operations.js
@@ -1,24 +1,21 @@
 import axios from "axios";
 import { getPostsAction } from "./actions";
 
-export const addPost = (tag_name, tagsArray, content, setShow, setLoading) => {
+export const addPost = (title, content, setShow, setLoading) => {
     return async(dispatch, getState) => {
-        if (tag_name !== undefined){
-            axios.post('api/post', { tag_name, tagsArray, content })
-                .then(res => {
-                    setShow(false);
-                    setLoading(false);
-                }).catch(res => {
-                    console.log(res);
-                });
-        }
+        axios.post('api/post', { title, content })
+            .then(res => {
+                setShow(false);
+                setLoading(false);
+            }).catch(res => {
+                console.log(res);
+            });
     }
 }
 
 export const getPosts = () => {
     return async(dispatch, getState) => {
         axios.get("api/posts").then(res => {
-            console.log(res);
             dispatch(getPostsAction({
                 post_array: res.data
             }))
