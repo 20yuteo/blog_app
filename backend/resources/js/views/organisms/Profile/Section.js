2794f0c38a1fc28d1175b55edb73b713d38d7c90
arakakitatsuki <20yuteo@gmail.com>
2021-08-15 18:51:58 +0900

    fix profile section compoent

--- backend/resources/js/views/organisms/Profile/Section.js
+++ backend/resources/js/views/organisms/Profile/Section.js
@@ -4,12 +4,11 @@ import Size from "../../styles/size";
 
 const Section = styled.section`
     z-index:2;
-    width: 80%;
-    height: 80%;
+    width: 90%;
+    height: 90%;
     padding: 1em;
     background:${ Color.Card.Background };
     color: ${ Color.Card.Headline };
     font-size: ${ Size.FontSize.Medium }rem;
-    overflow: scroll;
 `;
 export default Section;
