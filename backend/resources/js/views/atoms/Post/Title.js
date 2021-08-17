d4ac3271916eba6e40157eed06fd77715a39bce5
arakakitatsuki <20yuteo@gmail.com>
2021-08-17 22:50:18 +0900

    fix font size at post title js

--- backend/resources/js/views/atoms/Post/Title.js
+++ backend/resources/js/views/atoms/Post/Title.js
@@ -2,9 +2,8 @@ import Size from "../../styles/size";
 import Color from "../../styles/color";
 import styled from "styled-components";
 
-const Title = styled.div`
+const Title = styled.h2`
     color: ${ Color.Headline };
-    font-size: ${ Size.FontSize.Large }rem;
     font-weight: ${ Size.FontWeight };
     border-bottom: 8px solid ${ Color.Headline };
     padding: 24px;
