b6feb8ecd2d53a20441dc662b4079b7f14c1ee46
arakakitatsuki <20yuteo@gmail.com>
2021-08-19 23:03:35 +0900

    fix to delete setShow function and add push function at operations js

--- backend/resources/js/reducks/posts/operations.js
+++ backend/resources/js/reducks/posts/operations.js
@@ -1,26 +1,29 @@
 import axios from "axios";
+import {push} from 'connected-react-router';
 import { getPostsAction } from "./actions";
 
-export const addPost = (title, content, setShow, setLoading) => {
+export const addPost = (title, content, setLoading) => {
     return async(dispatch, getState) => {
         axios.post('api/post', {title, content })
             .then(res => {
-                setShow(false);
                 setLoading(false);
+                dispatch(push('/post'));
             }).catch(res => {
                 console.log(res);
+                dispatch(push('/post'));
             });
     }
 }
 
-export const updatePost = (id, title, content, setShow, setLoading) => {
+export const updatePost = (id, title, content, setLoading) => {
     return async(dispatch, getState) => {
         axios.put('api/post', {id, title, content })
             .then(res => {
-                setShow(false);
                 setLoading(false);
+                dispatch(push('/post'));
             }).catch(res => {
                 console.log(res);
+                dispatch(push('/post'));
             });
     }
 }
