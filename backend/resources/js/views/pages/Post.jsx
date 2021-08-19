1d842915b02a83af0fa468bc436ddd01af53c95f
arakakitatsuki <20yuteo@gmail.com>
2021-08-19 23:56:58 +0900

    delete page title component at post component

--- backend/resources/js/views/pages/Post.jsx
+++ backend/resources/js/views/pages/Post.jsx
@@ -1,4 +1,3 @@
-import PageTitle from "../atoms/Title/PageTitle"
 import BaseSection from "../organisms/Section/BaseSection";
 import NewPostLink from "../atoms/Links/NewPostLink";
 import { useSelector } from "react-redux";
@@ -11,9 +10,6 @@ const Post = () => {
     return (
         <>
             <BaseSection>
-                <PageTitle>
-                    Post
-                </PageTitle>
                 <NewPostLink to='/create'>New Post</NewPostLink>
             </BaseSection>
             <PostCardWrapper post_array={selector.posts.post_array} />
