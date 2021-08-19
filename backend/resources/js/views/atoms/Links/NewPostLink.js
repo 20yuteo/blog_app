9806b62a4eabc7d3c780cbcac394f219565c1db0
arakakitatsuki <20yuteo@gmail.com>
2021-08-19 23:35:02 +0900

    fix new post link js

--- backend/resources/js/views/atoms/Links/NewPostLink.js
+++ backend/resources/js/views/atoms/Links/NewPostLink.js
@@ -1,16 +1,18 @@
 import styled from "styled-components";
 import Color from "../../styles/color";
 import Size from "../../styles/size";
+import { Link } from "react-router-dom";
 
-const NewPostLink = styled.a`
+const NewPostLink = styled(Link)`
     background-color: ${ Color.Tertiary };
     color: ${Color.ButtonText};
     font-size: ${Size.FontSize.Medium}rem;
     font-weight: ${Size.Form.FontWeight};
     display: inline-block;
-    padding: 1.2rem 2rem;
+    padding: 2rem 0 2rem;
     min-height: 2rem;
     width: 80%;
+    margin: 10%;
     text-decoration: none;
     text-align: center;
 `;
