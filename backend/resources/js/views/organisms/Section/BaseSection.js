ee4c0915a186d5a4eeaea7058106878541025f63
arakakitatsuki <20yuteo@gmail.com>
2021-08-19 23:36:31 +0900

    fix base section js

--- backend/resources/js/views/organisms/Section/BaseSection.js
+++ backend/resources/js/views/organisms/Section/BaseSection.js
@@ -1,18 +1,8 @@
 import styled from "styled-components";
-import Size from "../../styles/size";
 
 const BaseSection = styled.section`
     justify-content: center;
     margin: 0 auto;
-    text-align: center;
-    @media screen and (max-width:${Size.MediaScreen.Small}px) {
-        max-width: 32rem;
-    }
-    @media screen and (min-width:${Size.MediaScreen.Small}px) and ( max-width:${Size.MediaScreen.Medium}px) {
-        max-width: 64rem;
-    }
-    @media screen and (min-width:${Size.MediaScreen.Large}px) {
-        max-width: 96rem;
-    }
+    width: 70vw;
 `;
 export default BaseSection;
