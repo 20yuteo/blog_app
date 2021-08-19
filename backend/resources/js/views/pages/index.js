62da30f0797a743bc61818eac677f4673ded71ba
arakakitatsuki <20yuteo@gmail.com>
2021-08-19 23:35:38 +0900

    add editor component at pages index js

--- backend/resources/js/views/pages/index.js
+++ backend/resources/js/views/pages/index.js
@@ -2,3 +2,4 @@ export {default as About} from './About';
 export {default as Home} from './Home';
 export {default as Login} from './Login';
 export {default as MyPage} from './MyPage';
+export {default as Editor} from './Editor';
