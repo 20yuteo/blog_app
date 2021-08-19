015b7b54e70f01b2fdb426f7bfd95cfad22245f3
arakakitatsuki <20yuteo@gmail.com>
2021-08-19 23:56:30 +0900

    delete page title component at login component

--- backend/resources/js/views/pages/Login.jsx
+++ backend/resources/js/views/pages/Login.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import PageTitle from '../atoms/Title/PageTitle';
 import LoginForm from '../organisms/LoginForm';
 
 const Login = () => {
     return (
         <div>
-            <PageTitle>
-                Login
-            </PageTitle>
             <LoginForm />
         </div>
     );
