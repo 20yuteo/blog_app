7080deee8cbf634a72ab714923c2215a0b23d17d
arakakitatsuki <20yuteo@gmail.com>
2021-09-07 22:49:40 +0900

    fix view pages index js

--- backend/resources/js/views/pages/index.js
+++ backend/resources/js/views/pages/index.js
@@ -2,3 +2,4 @@ export {default as Home} from './Home';
 export {default as Login} from './Login';
 export {default as MyPage} from './MyPage';
 export {default as Editor} from './Editor';
+export {default as MyProfile} from './MyProfile';
