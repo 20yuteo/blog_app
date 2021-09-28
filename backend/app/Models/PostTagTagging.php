ab9af43ae031288c7a67887209e3b9b0988dbeac
arakakitatsuki <20yuteo@gmail.com>
2021-09-28 22:15:30 +0900

    add post tag tagging model

--- backend/app/Models/PostTagTagging.php
+++ backend/app/Models/PostTagTagging.php
@@ -9,6 +9,12 @@ class PostTagTagging extends Model
 {
     use HasFactory;
 
+    protected $table = 'post_tag_tagging';
+
+    protected $fillable = [
+        'tag_id'
+    ];
+
     public function posts(){
         return $this->belongsTo('App\Models\Post');
     }
