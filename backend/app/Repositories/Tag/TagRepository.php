c3796b7ca7c96d282ee71824000874feee7b17d1
arakakitatsuki <20yuteo@gmail.com>
2021-09-28 22:12:39 +0900

    add tag repository class

--- backend/app/Repositories/Tag/TagRepository.php
+++ backend/app/Repositories/Tag/TagRepository.php
@@ -15,4 +15,15 @@ class TagRepository implements TagRepositoryInterface {
     public function getAllTag(){
         return $this->tag->all()->toArray();
     }
+
+    public function getTagByName(string $name) {
+        return Tag::where('name', $name)->get();
+    }
+
+    public function getTagByArray($array){
+        if (!empty($array)){
+            return Tag::whereIn('name', $array)->get();
+        }
+        return null;
+    }
 }
