bc42acce3d7d3b700525a853896c88457727d043
arakakitatsuki <20yuteo@gmail.com>
2021-09-28 22:16:21 +0900

    add tag factory to post repository class

--- backend/app/Repositories/Post/PostRepository.php
+++ backend/app/Repositories/Post/PostRepository.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Factories\PostFactory;
+use App\Factories\TagFactory;
 
 class PostRepository implements PostRepositoryInterface
 {
@@ -13,11 +14,15 @@ class PostRepository implements PostRepositoryInterface
 
     protected $post_factory;
 
-    public function __construct(Post $post, PostFactory $post_factory)
+    protected $tag_factory;
+
+    public function __construct(Post $post, PostFactory $post_factory, TagFactory $tag_factory)
     {
         $this->post = $post;
 
         $this->post_factory = $post_factory;
+
+        $this->tag_factory = $tag_factory;
     }
 
     /**
@@ -27,9 +32,14 @@ class PostRepository implements PostRepositoryInterface
      */
     public function savePost(Request $request)
     {
-        return Auth::user()->posts()->save(
+        $post = Auth::user()->posts()->save(
                 $this->post_factory->create($request)
             );
+
+        $tags = $this->tag_factory->create($request);
+
+        return $post->tags()->sync($tags->pluck('id')->toArray());
+
     }
 
     /**
@@ -39,7 +49,11 @@ class PostRepository implements PostRepositoryInterface
      */
     public function updatePost(Post $post, Request $request)
     {
-        return $post->fill($request->all())->save();
+        $post->fill($request->all())->save();
+
+        $tags = $this->tag_factory->create($request);
+
+        return $post->tags()->sync($tags->pluck('id')->toArray());
     }
 
     /**
@@ -49,6 +63,6 @@ class PostRepository implements PostRepositoryInterface
      */
     public function getAllPosts()
     {
-        return $this->post->all()->toArray();
+        return $this->post->with('tags')->get()->toArray();
     }
 }
